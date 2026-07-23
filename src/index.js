d1bc215cc00b02b721121e418dc55a7fa914e326
Shwetank-Maurya <sd3564086@gmail.com>
2026-07-23 18:20:58 +0530

    added tweet with hashtag processing

--- src/index.js
+++ src/index.js
@@ -7,24 +7,17 @@ const app = express();
 const Tweet = require('./models/tweet');
 const Comment = require('./models/comment');
 
+const TweetService = require('./services/tweet-service');
+
 const TweetRepository = require('./repository/tweet-repository');
+const HashtagRepository = require('./repository/hashtag-repository');
+const Hashtag = require('./models/hashtags');
 
 app.listen(3000, async () => {
     console.log(`Server started at PORT:3000`);
     await connect();
     console.log('Mongo DB connected');
     
-    // const tweetRepo = new TweetRepository();
-    // const tweet = await tweetRepo.create({content:'Tweet with comment Schema'});
-    // const comment = await Comment.create({content:'new comment'});
-    // tweet.comments.push(comment);
-    // await tweet.save();
-
-    const tweetRepo = new TweetRepository();
-    const tweet = await tweetRepo.create({content:'With hooks'});
-    console.log(tweet);
-    // const tweet = await tweetRepo.getAll(2,4);
-    // console.log(tweet);
 });
 
 
--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -19,16 +19,16 @@ class TweetRepository{
         }
     }
 
-    async update(tweetId,data){
-        try {
-            const tweet = await Tweet.findByIdAndUpdate(tweetId,data);
-            return tweet;
-        } catch (error) {
-            console.log(error);
+    // async update(tweetId,data){
+    //     try {
+    //         const tweet = await Tweet.findByIdAndUpdate(tweetId,data);
+    //         return tweet;
+    //     } catch (error) {
+    //         console.log(error);
             
-        }
+    //     }
         
-    }
+    // }
 
     async destroy(id){
         try {
