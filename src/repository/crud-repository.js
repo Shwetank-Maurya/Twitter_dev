bbecd5b52e1cceae6fd8bb684672472cbde4818c
Shwetank-Maurya <sd3564086@gmail.com>
2026-07-28 17:45:14 +0530

    Added the basic crud repo and extended it to the multiple classes

---
+++ src/repository/crud-repository.js
@@ -0,0 +1,59 @@
+class CrudRepository{
+    constructor(model){
+        this.model = model;
+    }
+
+    async create(data){
+        try {
+            const result = await this.model.create(data);
+            return result;
+        } catch (error) {
+            console.log("something went wrong in the crud repo");
+            throw error;
+        }
+    }
+
+    async destroy(id){
+        try {
+            const result = await this.model.findByIdAndDelete(id);
+            return result;
+            
+        } catch (error) {
+            console.log("Something went wrong in the crud repository");
+            throw error;
+        }
+    }
+
+    async get(id){
+        try {
+            const result = await this.model.findById(id);
+            return result;
+        } catch (error) {
+            console.log("Something went wrong in the crud repo");
+            throw error;
+        }
+    }
+
+    async getAll(id){
+        try {
+            const result = await this.model.find({});
+            return result;
+        } catch (error) {
+            console.log("SOmething went wrong in the crud repo");
+            throw error;
+        }
+    }
+
+    async update(id,data){
+        try {
+            const result = await this.model.findById(id,data,{new:true});
+            return result;
+        } catch (error) {
+            console.log("Something went wrong in the crud repo");
+            throw error;
+        }
+    }
+
+}
+
+export default CrudRepository;
